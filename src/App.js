55fdf91280e4a12665a2fa652eeeeac6e5ae7a74
Him-dhiman <62401406+Him-dhiman@users.noreply.github.com>
2022-06-06 22:53:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import { MDBContainer } from "mdb-react-ui-kit";
+
+export default function App() {
+  return <Footer />;
+}
